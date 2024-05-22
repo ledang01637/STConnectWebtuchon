311493d71769cdc800c8e10b6331b8e84a4d3036
ledang01637 <ledang01637@gmail.com>
2024-05-22 10:44:09 +0700

    Done

--- WebTuChon/WebTuChon/Controllers/HomeController.cs
+++ WebTuChon/WebTuChon/Controllers/HomeController.cs
@@ -1,30 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Mvc;
+using WebTuChon.Models;
 
 namespace WebTuChon.Controllers
 {
     public class HomeController : Controller
     {
+        private List<User> _users
+        {
+            get
+            {
+                if (Session["User"] == null)
+                {
+                    Session["User"] = new List<User>()
+                    {
+                        new User{Id = 1, FullName = "Nguyen Van A",Country = "VietNam", BirthDay = new DateTime(), Subject = new List<string>{" CNTT ", " C# " } }
+                    };
+                }
+                return (List<User>)Session["User"];
+            }
+            set
+            {
+                Session["User"] = value;
+            }
+        }
         public ActionResult Index()
         {
-            return View();
+            return View(_users);
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Create(User user)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            if(ModelState.IsValid)
+            {
+                _users.Add(user);
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ WebTuChon/WebTuChon/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebTuChon.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Country { get; set; }
+        public List<string> Subject {  get; set; }
+        public DateTime BirthDay { get; set; }
+
+    }
+}
